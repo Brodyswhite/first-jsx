7d922c22ce520083021e57de3478d3578cbeeb7f
Brody White <brodywhite@Brodys-MacBook-Air.local>
2020-10-29 14:45:54 -0500

    First commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+  <div>
+    <h1>Hello Dojo!</h1>
+    <h2>To Do List:</h2>
+      <ul>* Learn React</ul>
+      <ul>* Climb Mt Everest</ul>
+      <ul>* Run a marathon</ul>
+      <ul>* Feed the dog</ul>
+
+  </div>
+  );
+}
+
+export default App;
